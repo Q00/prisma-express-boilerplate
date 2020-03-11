06eb6a95d4205f5f04e63d800739e2e00eea71b6
Q00 <jqyu.lee@gmail.com>
2020-03-11 18:52:13 +0900

    test: 🔍 user test

--- src/test/acceptance_tests/unit_tests/userService.spec.ts
+++ src/test/acceptance_tests/unit_tests/userService.spec.ts
@@ -1,8 +1,34 @@
-import Container from 'typedi';
-import { UserService } from '../../../services/userService';
-import { User } from '../../../models/user';
+import { UserService } from '../../../services/UserService';
+import { User } from '../../../models/User';
+import { UserRepository } from '../../../contracts/UserRepository';
 test('getArticles_noArticlesInDB_emptyList', async () => {
-  const userService: UserService = Container.get('UserService');
+  const userService = new UserService(new EmptyMockRepository());
   const users: User[] = await userService.getUsers();
   expect(users.length).toBe(0);
 });
+
+test('getArticles_oneArticleInDB_ListOfOneArticle', async () => {
+  const userService = new UserService(new SingleMockRepository());
+  const users: User[] = await userService.getUsers();
+  expect(users.length).toBe(1);
+});
+
+class EmptyMockRepository implements UserRepository {
+  async users(): Promise<User[]> {
+    return [];
+  }
+}
+
+class SingleMockRepository implements UserRepository {
+  async users(): Promise<User[]> {
+    return [
+      {
+        nickname: 'Q00',
+        birthday: new Date('1995-03-27'),
+        sex: 'male',
+        userAccount: {},
+        userPushId: '123',
+      } as User,
+    ];
+  }
+}
