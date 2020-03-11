4a893e89f66b6c5cc98306982b996ef13f73e3be
Q00 <jqyu.lee@gmail.com>
2020-03-11 15:56:10 +0900

    test: 🔍 update user spec test

--- src/test/acceptance_tests/user.spec.ts
+++ src/test/acceptance_tests/user.spec.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../server';
+import { User } from '../../models/User';
 
 describe('GET /users', () => {
   it('SHOULD return 200Ok', (done) => {
@@ -13,8 +14,20 @@ describe('GET /users', () => {
 
   it('SHOULD return list of 2 users WHEN db containes 2 users', async (done) => {
     const users = [
-      new User('Q00', new Date('1995-03-27'), 'male', null, null),
-      new User('dahee', new Date('1995-01-27'), 'female', null, null),
+      {
+        nickname: 'Q00',
+        birthday: new Date('1995-03-27'),
+        sex: 'male',
+        userAccount: {},
+        userPushId: '123',
+      } as User,
+      {
+        nickname: 'dahee',
+        birthday: new Date('1995-01-27'),
+        sex: 'female',
+        userAccount: {},
+        userPushId: '1234',
+      } as User,
     ];
     await db.createArticle(users[0]);
     await db.createArticle(users[1]);
